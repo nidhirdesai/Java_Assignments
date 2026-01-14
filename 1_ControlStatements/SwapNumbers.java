f9a4ee4ea0195d367b65c7c469593ee578692e1c
nidhirdesai <nidhidesai752@gmail.com>
2026-01-14 13:03:13 +0530

    Create SwapNumbers.java

---
+++ 1_ControlStatements/SwapNumbers.java
@@ -0,0 +1,23 @@
+package practiceprograms;
+
+public class Swap2numbers {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // Swapping logic
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
