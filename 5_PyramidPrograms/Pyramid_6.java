4ed0f22843833674df8b1e72c163de32b0ed360d
nidhirdesai <nidhidesai752@gmail.com>
2026-01-20 23:26:22 +0530

    Create Pyramid_6.java

---
+++ 5_PyramidPrograms/Pyramid_6.java
@@ -0,0 +1,19 @@
+package practiceprograms1;
+
+public class pyramid6 {
+
+	public static void main(String[] args) {
+		 // Outer loop for rows
+        for (int r = 1; r <= 5; r++) {
+
+            // Inner loop prints numbers from 1 to r
+            for (int c = 1; c <= r; c++) //Row 3 → prints 1 2 3
+            {
+                System.out.print(c + " ");
+            }
+
+            // Go to next line
+            System.out.println();
+        }
+    }
+}
