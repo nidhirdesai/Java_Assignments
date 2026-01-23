0c2c376179ba98c2291c7da8dee6bd48da249880
nidhirdesai <nidhidesai752@gmail.com>
2026-01-23 21:19:38 +0530

    Create BubbleSort.java

---
+++ 6_ArrayAssignment/BubbleSort.java
@@ -0,0 +1,36 @@
+/*OUTPUT:
+Sorted Array (Ascending Order):
+2 3 4 5 8 
+*/
+
+package arrayprograms;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        // Declare and initialize the array
+        int[] arr = {5, 3, 8, 4, 2};
+
+        
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                // Compare current element with next element
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];        
+                    arr[j] = arr[j + 1];      
+                    arr[j + 1] = temp;       
+                }
+            }
+        }
+
+        // Print the sorted array
+        System.out.println("Sorted Array (Ascending Order):");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
