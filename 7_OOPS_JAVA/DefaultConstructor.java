23e6a6d349f8cee1e4e0b18b2a6e614b768365c7
nidhirdesai <nidhidesai752@gmail.com>
2026-02-02 09:44:47 +0530

    Create DefaultConstructor.java

---
+++ 7_OOPS_JAVA/DefaultConstructor.java
@@ -0,0 +1,17 @@
+package oopsprograms;
+
+class DefaultConstructor {
+
+    // Default Constructor
+    DefaultConstructor() 
+    {
+        System.out.println("Default constructor called");
+    }
+
+    // Main Method
+    public static void main(String[] args) {
+
+        DefaultConstructor obj = new DefaultConstructor();
+        System.out.print("End of program");
+    }
+}
