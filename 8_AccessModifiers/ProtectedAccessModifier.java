0822a225443f9b038fcbc8fbb8b29bd999d5f28d
nidhirdesai <nidhidesai752@gmail.com>
2026-02-02 18:58:42 +0530

    Create ProtectedAccessModifier.java

---
+++ 8_AccessModifiers/ProtectedAccessModifier.java
@@ -0,0 +1,29 @@
+
+package oopsprograms;
+
+public class ExampleClass {
+
+    public int number = 10;
+
+    public void show() {
+        System.out.println("Number: " + number);
+    }
+}
+package oopsprograms;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        ExampleClass obj = new ExampleClass();
+        
+        PrivateAccessM obj1 = new PrivateAccessM();
+        
+        obj1.show();
+        System.out.print("Number: "+obj1.number);
+        System.out.println("Number: "+obj1.num1);
+        System.out.println("Number: "+obj1.num2);
+        System.out.println("Number: "+obj1.num3);
+        obj.show();
+    }
+}
