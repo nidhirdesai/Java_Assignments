f2f0af5551ec9e79f0bafeb2b22a7f947fdf3d05
nidhirdesai <nidhidesai752@gmail.com>
2026-02-02 18:57:52 +0530

    Create PublicAccessModifier.java

---
+++ 8_AccessModifiers/PublicAccessModifier.java
@@ -0,0 +1,18 @@
+
+package oopsprograms;
+
+public class PublicAccessM {
+
+    public int number = 10;
+
+    public void show() {
+        System.out.println("Public Number: " + number);
+    }
+
+    public static void main(String[] args) {
+
+    	PublicAccessM obj = new PublicAccessM();
+
+        obj.show();
+    }
+}
