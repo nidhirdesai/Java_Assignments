e53b78f50dd76cc308de0308f2d4f454982187a9
nidhirdesai <nidhidesai752@gmail.com>
2026-02-09 19:13:57 +0530

    Create ArithmeticException.java

---
+++ 9_ExceptionHandling/ArithmeticException.java
@@ -0,0 +1,22 @@
+package exceptionhandling;
+
+public class EH1 {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b = 0;
+		try
+		{
+			int result = a/b;
+			System.out.println("Result = " + result);
+		}
+		catch (ArithmeticException e)
+		{
+			System.out.println("Error: Cannot divide by zero");
+		}
+		System.out.println("Program continues...");
+
+	}
+
+}
