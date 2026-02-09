a5b731e35c0eb92ff909979d17f50018a73a70fb
nidhirdesai <nidhidesai752@gmail.com>
2026-02-09 19:15:50 +0530

    Create NullPointerException.java

---
+++ 9_ExceptionHandling/NullPointerException.java
@@ -0,0 +1,18 @@
+package exceptionhandling;
+
+public class EH4 {
+
+	public static void main(String[] args) {
+		
+		String s = null;
+		try {
+			System.out.print(s.length());
+		}
+		catch(NullPointerException e)
+		{
+			 System.out.println("String is null. Cannot find length");
+		}
+
+	}
+
+}
