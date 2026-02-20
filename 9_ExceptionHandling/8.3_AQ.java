cf85ee7b453af4e03b233af697243199ad3235ad
nidhirdesai <nidhidesai752@gmail.com>
2026-02-20 17:33:16 +0530

    Create 8.3_AQ.java

---
+++ 9_ExceptionHandling/8.3_AQ.java
@@ -0,0 +1,18 @@
+package exceptionhandling;
+
+public class EH3 {
+
+	public static void main(String[] args) {
+		
+		String s = "Hello";
+		try {
+			int num = Integer.parseInt(s);
+			 System.out.println("Number = " + num);
+		}
+        catch(NumberFormatException e)
+		{
+        	System.out.println("Cannot convert String to Number");
+		}
+	}
+}
+//Cannot convert String to Number
