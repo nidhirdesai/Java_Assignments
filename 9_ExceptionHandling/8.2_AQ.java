c3ed5aab5f6470b2f34245e03016a285ce1004dd
nidhirdesai <nidhidesai752@gmail.com>
2026-02-20 17:32:17 +0530

    Create 8.2_AQ.java

---
+++ 9_ExceptionHandling/8.2_AQ.java
@@ -0,0 +1,37 @@
+package exceptionhandling;
+
+import java.util.Scanner;
+
+public class EH2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a[] = new int[5];  // Size = 5 (Index 0 to 4)
+		
+		System.out.print("Enter position (0-4): ");
+		int pos = sc.nextInt();
+		
+		System.out.print("Enter value: ");
+		int value=sc.nextInt();
+		
+		try
+		{
+			a[pos]=value;
+			System.out.println("Value stored: " + a[pos]);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			 System.out.println("Invalid Index! Enter between 0 and 4");
+		}
+		
+		sc.close();
+
+	}
+
+}
+/*
+Enter position (0-4): 5
+Enter value: 10
+Invalid Index! Enter between 0 and 4 */
