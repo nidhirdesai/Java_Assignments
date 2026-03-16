d657ee617c3a47e181c415041045b6441befc60e
nidhirdesai <nidhidesai752@gmail.com>
2026-03-16 19:05:29 +0530

    Create Q_1.3.java

---
+++ 12_Practical_Assessment_16-03-2026/Q_1.3.java
@@ -0,0 +1,23 @@
+package seleniumassignment;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class pa3 {
+
+	public static void main(String[] args) {
+		
+WebDriver driver = new ChromeDriver();
+		
+		
+driver.manage().window().maximize();
+		
+		driver.get("https://www.leafground.com/dashboard.xhtml");
+		
+		 driver.findElement(By.xpath("//select[@class='ui-selectonemenu']")).click();
+
+	}
+
+}
